8f871012d7e7d52e2648c264da1eaac822d8d008
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-21 19:39:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,17 @@
 def starts_with_a_vowel?(word)
-
+ if word.match(/\b[aeiouAEIOU]/)
+    return true
+  else 
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan(/\b(un...ing/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_five_letters_long(text)
@@ -15,5 +23,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  phone
 end
